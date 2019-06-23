a983c5171b711a4da60f7fcb431979cbc0e0f734
Mr.Po <ldd_live@foxmailcom>
2019-06-23 14:09:59 +0800

    增加注释

--- app.js
+++ app.js
@@ -39,7 +39,7 @@
         /**
          * 媒体类型
          *
-         * 可通过修改此间数据，影响资源名称中的 @mdeia_type 参数
+         * 可通过修改此间数据，影响资源名称中的 @mdeia_type 参数值
          */
         static get mediaType() {
             return {
@@ -51,13 +51,20 @@
 
         /**
          * 得到资源名称
+         * 可通过调配、增删其返回值，修改资源名称
+         *
+         * 默认的：${wb_user_name}-${wb_id}-${no}
+         * 会生成：小米商城-4375413591293810-01
          * 
-         * @param  {字符串} wb_user_name 微博用户名
-         * @param  {字符串} wb_user_id   微博用户ID
-         * @param  {字符串} wb_id        微博ID
-         * @param  {字符串} resource_id  资源原始名称
-         * @param  {字符串} no           序号
-         * @param  {字符串} mdeia_type   媒体类型
+         * 若改为：微博-${media_type}-${wb_user_name}-${wb_id}-${no}
+         * 会生成：微博-P-小米商城-4375413591293810-01
+         * 
+         * @param  {字符串} wb_user_name 微博用户名（如：小米商城）
+         * @param  {字符串} wb_user_id   微博用户ID（如：5578564422）
+         * @param  {字符串} wb_id        微博ID（如：4375413591293810）
+         * @param  {字符串} resource_id  资源原始名称（如：0065x5rwly1g3c6exw0a2j30u012utyg）
+         * @param  {字符串} no           序号（如：01）
+         * @param  {字符串} mdeia_type   媒体类型（如：P）
          * 
          * @return {字符串}              由以上字符串组合而成的名称
          */
@@ -67,10 +74,18 @@
 
         /**
          * 得到打包名称
+         * 可通过调配、增删其返回值，修改打包名称
+         *
+         * 默认的：${wb_user_name}-${wb_id}
+         * 会生成：小米商城-4375413591293810
+         *
+         * 若改为：压缩包-${wb_user_name}-${wb_id}
+         * 会生成：压缩包-小米商城-4375413591293810
          * 
-         * @param  {字符串} wb_user_name 微博用户名
-         * @param  {字符串} wb_user_id   微博用户ID
-         * @param  {字符串} wb_id        微博ID
+         * 
+         * @param  {字符串} wb_user_name 微博用户名（如：小米商城）
+         * @param  {字符串} wb_user_id   微博用户ID（如：5578564422）
+         * @param  {字符串} wb_id        微博ID（如：4375413591293810）
          * 
          * @return {字符串}              由以上字符串组合而成的名称
          */
--- bin/Config.js
+++ bin/Config.js
@@ -7,7 +7,7 @@ class Config {
     /**
      * 媒体类型
      *
-     * 可通过修改此间数据，影响资源名称中的 @mdeia_type 参数
+     * 可通过修改此间数据，影响资源名称中的 @mdeia_type 参数值
      */
     static get mediaType() {
         return {
@@ -19,13 +19,20 @@ class Config {
 
     /**
      * 得到资源名称
+     * 可通过调配、增删其返回值，修改资源名称
+     *
+     * 默认的：${wb_user_name}-${wb_id}-${no}
+     * 会生成：小米商城-4375413591293810-01
      * 
-     * @param  {字符串} wb_user_name 微博用户名
-     * @param  {字符串} wb_user_id   微博用户ID
-     * @param  {字符串} wb_id        微博ID
-     * @param  {字符串} resource_id  资源原始名称
-     * @param  {字符串} no           序号
-     * @param  {字符串} mdeia_type   媒体类型
+     * 若改为：微博-${media_type}-${wb_user_name}-${wb_id}-${no}
+     * 会生成：微博-P-小米商城-4375413591293810-01
+     * 
+     * @param  {字符串} wb_user_name 微博用户名（如：小米商城）
+     * @param  {字符串} wb_user_id   微博用户ID（如：5578564422）
+     * @param  {字符串} wb_id        微博ID（如：4375413591293810）
+     * @param  {字符串} resource_id  资源原始名称（如：0065x5rwly1g3c6exw0a2j30u012utyg）
+     * @param  {字符串} no           序号（如：01）
+     * @param  {字符串} mdeia_type   媒体类型（如：P）
      * 
      * @return {字符串}              由以上字符串组合而成的名称
      */
@@ -35,10 +42,18 @@ class Config {
 
     /**
      * 得到打包名称
+     * 可通过调配、增删其返回值，修改打包名称
+     *
+     * 默认的：${wb_user_name}-${wb_id}
+     * 会生成：小米商城-4375413591293810
+     *
+     * 若改为：压缩包-${wb_user_name}-${wb_id}
+     * 会生成：压缩包-小米商城-4375413591293810
      * 
-     * @param  {字符串} wb_user_name 微博用户名
-     * @param  {字符串} wb_user_id   微博用户ID
-     * @param  {字符串} wb_id        微博ID
+     * 
+     * @param  {字符串} wb_user_name 微博用户名（如：小米商城）
+     * @param  {字符串} wb_user_id   微博用户ID（如：5578564422）
+     * @param  {字符串} wb_id        微博ID（如：4375413591293810）
      * 
      * @return {字符串}              由以上字符串组合而成的名称
      */
